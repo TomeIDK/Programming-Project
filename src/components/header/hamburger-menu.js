9062652cd978dfe7e0cacb51a517822eb821f1b8
tomeidk <cecepas2001@gmail.com>
2024-04-17 15:05:24 +0200

    Combined Header + Menu

---
+++ src/components/header/hamburger-menu.js
@@ -0,0 +1,19 @@
+const btnClose = document.getElementById("nav-close");
+const body = document.getElementById("body");
+const nav = document.getElementById("nav-menu");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  nav.classList.remove("hidden");
+  body.classList.remove("unblur");
+  body.classList.add("blur");
+  nav.classList.remove("inactive");
+  nav.classList.add("active");
+});
+
+btnClose.addEventListener("click", () => {
+  body.classList.add("unblur");
+  body.classList.remove("blur");
+  nav.classList.add("inactive");
+  nav.classList.remove("active");
+});
