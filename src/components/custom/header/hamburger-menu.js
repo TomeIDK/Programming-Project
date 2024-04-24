86ab53211914dcccc53a20d2b40b00bf08c27908
tomeidk <cecepas2001@gmail.com>
2024-04-24 14:34:05 +0200

    Disabled scrolling on body while menu is open

--- src/components/custom/header/hamburger-menu.js
+++ src/components/custom/header/hamburger-menu.js
@@ -1,5 +1,6 @@
 const btnClose = document.getElementById("nav-close");
 const body = document.body;
+const html = document.documentElement;
 const nav = document.getElementById("nav-menu");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
@@ -9,6 +10,7 @@ hamburgerMenu.addEventListener("click", () => {
   body.classList.add("blur");
   nav.classList.remove("inactive");
   nav.classList.add("active");
+  html.style.overflow = "hidden";
 });
 
 btnClose.addEventListener("click", () => {
@@ -16,6 +18,7 @@ btnClose.addEventListener("click", () => {
   body.classList.remove("blur");
   nav.classList.add("inactive");
   nav.classList.remove("active");
+  html.style.overflow = "visible";
   setTimeout(() => {
     nav.classList.add("hidden");
   }, 450)
