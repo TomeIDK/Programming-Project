724bcc66723db5c1ee6623fbeb6a3b509d33b8e3
NehadTbk <nehad.tabbakh@gmail.com>
2024-04-30 13:38:30 +0200

    MySQl

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -8,4 +9,21 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
+=======
+import mysql from'mysql';
+
+const connection = mysql.createConnection({
+    host: '', // Change this to your MySQL host
+    user: '2324PROGPRGR10',      // Your MySQL username
+    password: 'hVWX33SX',      // Your MySQL password
+    database: '2324PROGPRGR10' // Your database name
+}).promise()
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database');
+>>>>>>> Stashed changes
 });
