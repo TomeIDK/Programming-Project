f4d8f23765aee112800f9c48bc2e6941c7d56d10
Tome <cecepas2001@gmail.com>
2024-04-30 14:24:41 +0200

    routing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import mysql from'mysql';
-const express = require('express');
+// import express from 'express';
+// import path from 'path';
+const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
+const path = require("path");
+// const mysql = require("mysql");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get('/info', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'info.html'));
@@ -15,22 +19,22 @@ app.get('/cataloog', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 
+  const connection = mysql
+    .createConnection({
+      host: "", // Change this to your MySQL host
+      user: "2324PROGPRGR10", // Your MySQL username
+      password: "hVWX33SX", // Your MySQL password
+      database: "2324PROGPRGR10", // Your database name
+    })
+    .promise();
 
-const connection = mysql.createConnection({
-    host: '', // Change this to your MySQL host
-    user: '2324PROGPRGR10',      // Your MySQL username
-    password: 'hVWX33SX',      // Your MySQL password
-    database: '2324PROGPRGR10' // Your database name
-}).promise()
-
-connection.connect((err) => {
+  connection.connect((err) => {
     if (err) {
-        console.error('Error connecting to database: ' + err.stack);
-        return;
+      console.error("Error connecting to database: " + err.stack);
+      return;
     }
-    console.log('Connected to database');
+    console.log("Connected to database");
+  });
 });
-});
-
--- src/routes/cataloog.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const path = require('path');
-
-app.get('/cataloog', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cataloog.html'));
-});
-
