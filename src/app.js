eea8dc2d64227df0f63641acc22b99c3f48b5322
Giles Synaeve <giles.synaeve@student.ehb.be>
2024-04-30 15:36:03 +0200

    mysql

--- src/app.js
+++ src/app.js
@@ -1,29 +1,18 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-const mysql = require('mysql');
-=======
 const mysql = require("mysql");
-// import express from 'express';
-// import path from 'path';
->>>>>>> Stashed changes
 const express = require("express");
-=======
-const mysql= require('mysql');
-const express = require('express');
->>>>>>> Stashed changes
+
 const app = express();
 const port = 3000;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get('/info', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'info.html'));
+app.get("/info", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "info.html"));
 });
 
-app.get('/cataloog', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cataloog.html'));
+app.get("/cataloog", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "cataloog.html"));
 });
 
 app.listen(port, () => {
@@ -34,8 +23,9 @@ app.listen(port, () => {
       host: "dt5.ehb.be", // Change this to your MySQL host
       user: "2324PROGPRGR10", // Your MySQL username
       password: "hVWX33SX", // Your MySQL password
-      database: "dt5.ehb.be", // Your database name
-    }).promise();
+      database: "2324PROGPRGR10", // Your database name
+    });
+   
 
   connection.connect((err) => {
     if (err) {
