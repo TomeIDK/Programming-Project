24495935dd26bba5f7c66a3e7439b4db449639ac
Tome <cecepas2001@gmail.com>
2024-04-30 12:22:36 +0200

    cataloog route added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/info', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'info.html'));
+});
+
+app.get('/cataloog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cataloog.html'));
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
