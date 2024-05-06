b62b39e7de07a839a8e9d0de8b533c0fa4fee2fe
NehadTbk <nehad.tabbakh@gmail.com>
2024-05-07 01:45:59 +0200

    API maken voor producten
    Route to get all product and reorganized code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
-const mysql = require("mysql");
 const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Routers
 const info = require("./routes/info");
@@ -16,23 +18,37 @@ app.use("/info", info);
 app.use("/cataloog", catalog);
 app.use("/product/:id", product);
 
+// MySQL connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, // Change this to your MySQL host
+  user: process.env.DB_USERNAME, // Your MySQL username
+  password: process.env.DB_PASSWORD, // Your MySQL password
+  database: process.env.DB_DATABASE, // Your database name
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database: " + err.stack);
+    return;
+  }
+  console.log("Connected to database");
+});
+
+// Route to get all products
+app.get("/api/products", (req, res) => {
+  const query = "SELECT * FROM products";
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error("Error getting products: " + error);
+      res.status(500).json({ error: "Error getting products" });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 // Listen for connections
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-
-  // MySQL connection
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST, // Change this to your MySQL host
-    user: process.env.DB_USERNAME, // Your MySQL username
-    password: process.env.DB_PASSWORD, // Your MySQL password
-    database: process.env.DB_DATABASE, // Your database name
-  });
-
-  connection.connect((err) => {
-    if (err) {
-      console.error("Error connecting to database: " + err.stack);
-      return;
-    }
-    console.log("Connected to database");
-  });
 });
+
