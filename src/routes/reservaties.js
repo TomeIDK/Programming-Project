aafeb8ced0bf7425c46d570a6223116756135dd5
Tome <cecepas2001@gmail.com>
2024-05-27 15:03:59 +0200

    Mijn reservaties annuleren button
    - Annuleer reservatie in /reservaties werkt nu
    - Verwijdert reservatie uit tabel uitlening via dbService
    - CSS en layout van pagina aangepast naar juiste design
    - favicon en title toegevoegd aan reservaties pagina

--- src/routes/reservaties.js
+++ src/routes/reservaties.js
@@ -1,46 +1,66 @@
 // routes/reservaties.js
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const router = express.Router();
 const mysql = require("mysql");
+const DBService = require("../dbService");
+let dbServiceInstance = new DBService();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 // Open connection to DB
-connection.connect((err)=>{
-    if (err){
-      console.error ("fout bij verbinden met DB: ", err);
-      return;
-    }
-    console.log("verbonden met DB");
-  });
+connection.connect((err) => {
+  if (err) {
+    console.error("fout bij verbinden met DB: ", err);
+    return;
+  }
+  console.log("verbonden met DB");
+});
 
 router.get("/", (req, res) => {
-    const userID = req.session.user.userID;
-    connection.query(
-        `SELECT U.startDatum, U.eindDatum, P.naam AS productNaam
+  const userID = req.session.user.userID;
+  connection.query(
+    `SELECT U.uitleningID, U.startDatum, U.eindDatum, P.naam AS productNaam
          FROM Uitlening U
          JOIN Product P ON U.artikelID = P.productID
          WHERE U.userID = ?`,
-        [userID],
-        (error, results) => {
-            if (error) {
-                console.error("Error executing query:", error);
-                res.status(500).send("Er is een fout opgetreden bij het ophalen van de reserveringen.");
-                return;
-            }
+    [userID],
+    (error, results) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        res
+          .status(500)
+          .send(
+            "Er is een fout opgetreden bij het ophalen van de reserveringen."
+          );
+        return;
+      }
 
-            console.log("Reservations fetched from database:", results);
+      console.log("Reservations fetched from database:", results);
 
-            res.render("reservaties", { reservations: results });
-        }
-    );
+      res.render("reservaties", { reservations: results });
+    }
+  );
+});
+
+router.delete("/", (req, res) => {
+    console.log("uitleningid: ", req.body.uitleningID);
+    console.log(req.body);
+  dbServiceInstance.cancelReservation(req.body.uitleningID, (error, result) => {
+    if (error) {
+      console.error("Kan reservatie niet annuleren: ", error);
+      res.status(400).send("Kan reservatie niet annuleren");
+      return;
+    } else {
+      console.log("Reservatie succesvol geannuleerd");
+      res.status(200).send("Reservatie succesvol geannuleerd");
+    }
+  });
 });
 
 module.exports = router;
