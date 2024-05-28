13d8976b972db4b1c6bb284168f9db4099b17a6b
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-05-28 14:21:13 +0200

    materiaal aanpasser pagina

---
+++ src/public/js/materiaal aanpassen.js
@@ -0,0 +1,7 @@
+document.getElementById("verwijderenBtn").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
+});
+
+document.getElementById("neeBtn").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+});
